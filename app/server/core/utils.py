d04b11a9941ab891ce7792232883c2f54710baf1
Tony Okeke <tonykabilanokeke@gmail.com>
2023-07-26 21:40:20 -0400

    define env variables

--- app/server/core/utils.py
+++ app/server/core/utils.py
@@ -5,11 +5,18 @@ Utility functions for the app
 import subprocess
 import logging
 import os
-from rich.logging import RichHandler
+
 from pathlib import Path
 from time import sleep
+
+
+# get directories
+RUN_DIR = Path(os.getenv("RUN_DIR", "/run"))
+SCRIPTS_DIR = Path(os.getenv("SCRIPTS_DIR", "/scripts"))
 
 
 def get_logger() -> logging.Logger:
     """Get logger"""
     return logging.getLogger("pfred")
+
+
