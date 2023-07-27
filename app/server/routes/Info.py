60c43551026d634e9cb08f99239f3c686a779d69
Tony Okeke <tonykabilanokeke@gmail.com>
2023-07-26 21:34:37 -0400

    add logger

--- app/server/routes/Info.py
+++ app/server/routes/Info.py
@@ -6,8 +6,11 @@ from fastapi import APIRouter, HTTPException
 from fastapi.responses import PlainTextResponse
 
 from .. import __version__
+from ..core import utils
+
 
 router = APIRouter()
+logger = utils.get_logger()
 
 
 @router.get(
@@ -18,9 +21,11 @@ router = APIRouter()
 async def get_version() -> Any:
     """Fetch service version"""
     try:
+        logger.info("Fetching service version")
         return __version__
 
     except Exception as exc:
+        logger.error("Error occured fetching service version")
         raise HTTPException(
             status_code=400, detail="Error occured fetching service version"
         ) from exc
