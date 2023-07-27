dd9cf6529d13c0e8598f18e64328cbd8ee36b8ce
Tony Okeke <tonykabilanokeke@gmail.com>
2023-07-26 20:26:00 -0400

    add title and description

--- app/server/app.py
+++ app/server/app.py
@@ -8,10 +8,17 @@ from .routes.ActivityModel import router as ActivityModelRouter
 from .routes.Info import router as InfoRouter
 from .routes.OffTargetSearch import router as OffTargetSearchRouter
 from .routes.ScriptUtilities import router as ScriptUtilitiesRouter
+from . import __version__
 
 
-# create app and add routes
-app = FastAPI()
+# create app
+app = FastAPI(
+    title="PFRED API",
+    description="RNAi Enumeration and Design",
+    version=__version__
+)
+
+# include routers
 app.include_router(ActivityModelRouter, tags=["ActivityModel"], prefix="/ActivityModel")
 app.include_router(InfoRouter, tags=["Info"], prefix="/Info")
 app.include_router(OffTargetSearchRouter, tags=["OffTargetSearch"], prefix="/OffTargetSearch")
