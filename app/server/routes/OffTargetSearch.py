97e430e4906f860e16de5d0fb19c7f57aa4eb6f7
Tony Okeke <tonykabilanokeke@gmail.com>
2023-07-27 04:46:32 -0400

    add /ASO

--- app/server/routes/OffTargetSearch.py
+++ app/server/routes/OffTargetSearch.py
@@ -62,23 +62,53 @@ async def run_sirna_search(
     return PlainTextResponse("siRNAOffTargetSearch.sh run failed", status_code=400)
 
 
-@router.get("/ASO", response_description="Run ASO Off Target Search")
+@router.get(
+    "/ASO",
+    response_description="Run ASO Off target search",
+    response_class=PlainTextResponse
+)
 async def run_aso_search(
-    species: str = Query(..., alias="Species"),
-    run_directory: str = Query(..., alias="RunDirectory"),
-    ids: str = Query(..., alias="IDs"),
-    missmatch: int = Query(..., alias="missMatches"),
-):
-    """Run ASO Off Target Search"""
-    try:
-        # TODO: Run the ASO Off Target Search here
-        # Use species, run_directory, ids, and missmatches in the model
+    species: str = Query(
+        ..., alias="Species", description="Species"
+    ),
+    run_name: str = Query(
+        ..., alias="RunDirectory", description="Run directory"
+    ),
+    ids: str = Query(
+        ..., alias="IDs", description="IDs"
+    ),
+    miss_matches: int = Query(
+        ..., alias="missMatches", description="number of allowed mismatches"
+    ),
+) -> Any:
+    """Run siRNA Off Target Search"""
 
-        return {"message": "Run ASO Off Target Search successfully"}
-    except Exception as exc:
-        raise HTTPException(
-            status_code=400, detail="Error occurred in running ASO Off Target Search"
-        ) from exc
+    # create run directory
+    run_directory = utils.create_run_dir(run_name)
+
+    #! run_directory must point to 'scripts/'
+    # create command
+    cmd = f"ASOOffTargetSearch.sh {species} {ids} {miss_matches}"
+
+    success = False
+
+    logger.info("Running ASO search for %s", species)
+
+    if species == "paco":
+        logger.info("Shell command avoided, skipping...")
+        success = True
+    else:
+        success = utils.run_shell(cmd, run_directory)
+
+    if success:
+        logger.info("siRNAOffTargetSearch.sh ran successfully")
+        try:
+            result = utils.read_file(os.path.join(run_directory, "aso_search_result.csv"))
+            return result
+        except Exception as exc:  # pylint: disable=broad-except
+            return PlainTextResponse(f"Error reading file: {exc}", status_code=400)
+
+    return PlainTextResponse("ASOOffTargetSearch.sh run failed", status_code=400)
 
 
 @router.get("/Check", response_description="Run Check file existence")
