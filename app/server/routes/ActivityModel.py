7ef2dac21648186667c20b921862d32e6247dbbf
Tony Okeke <tonykabilanokeke@gmail.com>
2023-07-27 05:17:34 -0400

    add /siRNA

--- app/server/routes/ActivityModel.py
+++ app/server/routes/ActivityModel.py
@@ -1,6 +1,10 @@
 """ActivityModel API endpoints"""
 
-from fastapi import APIRouter, HTTPException, Query
+from typing import Any
+import os
+
+from fastapi.responses import PlainTextResponse
+from fastapi import APIRouter, Query
 
 from ..core import utils
 
@@ -9,22 +13,52 @@ router = APIRouter()
 logger = utils.get_logger()
 
 
-@router.post("/siRNA")
-async def run_sirna(
-    primary_id: str = Query(..., alias="PrimaryID"),
-    run_directory: str = Query(..., alias="RunDirectory"),
-):
+@router.post(
+    "/siRNA",
+    responses={
+        200: {"description": "Run siRNA activity model successfully"}, 
+        400: {"description": "Error occurred in running siRNA activity model"}
+    },
+    response_class=PlainTextResponse
+)
+async def run_sirna_activity_model(
+    primary_id: str = Query(..., alias="PrimaryID", description="Primary ID"),
+    run_name: str = Query(..., alias="RunDirectory", description="Run directory")
+) -> Any:
     """Run siRNA activity model"""
-    try:
-        # TODO: Run the siRNA activity model here
-        # Use primary_id and run_directory in the model
 
-        return {"message": "Run siRNA activity model successfully"}
+    # create run directory
+    run_directory = utils.create_run_dir(run_name)
 
-    except Exception as exc:
-        raise HTTPException(
-            status_code=400, detail="Error occurred in running siRNA activity model"
-        ) from exc
+    # get script path
+    scripts_dir = utils.get_scripts_dir()
+
+    #! run_directory must point to 'scripts/'
+    #! scripts_dir must contain 'siRNA_2431seq_modelBuilding.csv'
+    # construct command
+    cmd  = f"getSeqGivenTrans.sh {primary_id}"
+
+    # run command
+    success = utils.run_shell(cmd, run_directory)
+
+    if success:
+        logger.info("getSeqGivenTrans.sh ran successfully")
+
+        # copy modelBuilding.csv to run_directory
+        utils.copyfile(os.path.join(scripts_dir, "siRNA_2431seq_modelBuilding.csv"), run_directory)
+
+        success = utils.run_shell("siRNAActivityModel.sh", run_directory)
+
+        if success:
+            logger.info("siRNAActivityModel.sh ran successfully")
+            try:
+                result = utils.read_file(os.path.join(run_directory, "siRNAActivityModel.csv"))
+                return result
+            except Exception as exc:  # pylint: disable=broad-except
+                logger.error("Error reading file: %s", exc)
+                return PlainTextResponse(f"Error reading file: {exc}", status_code=400)
+
+    return PlainTextResponse("getSeqGivenTrans.sh run failed", status_code=400)
 
 
 @router.post("/ASO")
