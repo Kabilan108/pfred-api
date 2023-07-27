54e85b699141fdbb7131b9e04fa086209266e311
Tony Okeke <tonykabilanokeke@gmail.com>
2023-07-26 21:32:11 -0400

    add logger for commands

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 """Runner for the FastAPI server."""
 
+import logging
 import uvicorn
+
+
+# set up logging
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    filename="pfred.log",
+    filemode="a",
+    datefmt="[ %X ]",
+)
+
+# create logger
+logger = logging.getLogger("pfred")
 
 
 if __name__ == "__main__":
