9de8a99afcedf508649810f32f64f966cd6f922d
Tony Okeke <tonykabilanokeke@gmail.com>
2023-07-27 05:23:43 -0400

    add /ASO

--- app/server/routes/ActivityModel.py
+++ app/server/routes/ActivityModel.py
@@ -61,20 +61,50 @@ async def run_sirna_activity_model(
     return PlainTextResponse("getSeqGivenTrans.sh run failed", status_code=400)
 
 
-@router.post("/ASO")
-async def run_aso(
-    primary_id: str = Query(..., alias="PrimaryID"),
-    run_directory: str = Query(..., alias="RunDirectory"),
-    oligo_length: int = Query(..., alias="OligoLength"),
-):
-    """Run ASO activity model"""
-    try:
-        # TODO: Run the ASO activity model here
-        # Use primary_id, run_directory, and oligo_length in the model
+@router.post(
+    "/ASO",
+    responses={
+        200: {"description": "Run ASO activity model successfully"},
+        400: {"description": "Error occurred in running siRNA activity model"}
+    },
+    response_class=PlainTextResponse
+)
+async def run_aso_activity_model(
+    primary_id: str = Query(..., alias="PrimaryID", description="Primary ID"),
+    run_name: str = Query(..., alias="RunDirectory", description="Run directory")
+) -> Any:
+    """Run siRNA activity model"""
 
-        return {"message": "Run ASO activity model successfully"}
+    # create run directory
+    run_directory = utils.create_run_dir(run_name)
 
-    except Exception as exc:
-        raise HTTPException(
-            status_code=400, detail="Error occurred in running ASO activity model"
-        ) from exc
+    # get script path
+    scripts_dir = utils.get_scripts_dir()
+
+    #! run_directory must point to 'scripts/'
+    #! scripts_dir must contain 'siRNA_2431seq_modelBuilding.csv'
+    # construct command``
+    cmd  = f"getSeqGivenTrans.sh {primary_id}"
+
+    # run command
+    success = utils.run_shell(cmd, run_directory)
+
+    if success:
+        logger.info("getSeqGivenTrans.sh ran successfully")
+
+        # copy files to run_directory
+        utils.copyfile(os.path.join(scripts_dir, "input_15_21_100_1000_12.txt"), run_directory)
+        utils.copyfile(os.path.join(scripts_dir, "AOBase_542seq_cleaned_modelBuilding_Jan2009_15_21_noOutliers.csv"), run_directory)
+
+        success = utils.run_shell("ASOActivityModel.sh", run_directory)
+
+        if success:
+            logger.info("ASOActivityModel.sh ran successfully")
+            try:
+                result = utils.read_file(os.path.join(run_directory, "ASOActivityModelResult.csv"))
+                return result
+            except Exception as exc:  # pylint: disable=broad-except
+                logger.error("Error reading file: %s", exc)
+                return PlainTextResponse(f"Error reading file: {exc}", status_code=400)
+
+    return PlainTextResponse("getSeqGivenTrans.sh run failed", status_code=400)
