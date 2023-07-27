d84afbe52edb57ad6f1140dd1ec3ce9ddd58fcb3
Tony Okeke <tonykabilanokeke@gmail.com>
2023-07-26 21:49:33 -0400

    add run_shell
    replacement for public static boolean runCommandThroughShell(String command, String directory)

--- app/server/core/utils.py
+++ app/server/core/utils.py
@@ -20,3 +20,20 @@ def get_logger() -> logging.Logger:
     return logging.getLogger("pfred")
 
 
+# create logger
+logger = logging.getLogger("pfred")
+
+
+# public static boolean runCommandThroughShell(String command, String directory)
+def run_shell(command: str, directory: str) -> bool:
+    """Run a command in the shell"""
+
+    logger.warning("Running command: %s in %s", command, directory)
+
+    try:
+        subprocess.run(f"cd {directory} && {command}", shell=True, check=True)
+    except subprocess.CalledProcessError as exc:
+        logger.error("Error executing command in bash shell: %s", exc)
+        return False
+
+    return True
