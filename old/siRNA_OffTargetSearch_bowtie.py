390ed13722c08262127407436911b47e70ff6297
Tony Okeke <tonykabilanokeke@gmail.com>
2023-07-31 11:10:44 -0400

    dump2

---
+++ old/siRNA_OffTargetSearch_bowtie.py
@@ -0,0 +1,11 @@
+import os
+import re
+
+# assuming toDNA() function is defined somewhere in the code
+def toDNA(seq):
+    # function implementation here
+    pass
+
+names = []
+usage = "usage message here"
+
